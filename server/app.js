25590b97901aec83eb367a5fc0ca4e08007e7a4b
Abhishek Raj <imabhishekraj8757@gmail.com>
2023-06-11 08:25:42 +0530

    added database config and initialisation

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(process.env.PORT,() => {
-    console.log(`Server is up and running on port ${process.env.PORT}`)
+app.get('/healthcheck',(req,res) => {
+    res.send('Server is up and running!');
+})
+
+app.listen(process.env.PORT, async () => {
+    await mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+     .then(() => console.log(`Server is listening on port ${process.env.PORT}`))
+     .catch((error) => console.log(error))
 })
